c0aa58ef5ec1638ba28325216d77f9683420057d
Wataru Sekiguchi <wataru.sekiguchi416@gmail.com>
2018-09-10 15:23:35 +0900

    Done

---
+++ app/src/main/java/com/example/watarusekiguchi/firstkotlinapp/SecondActivity.kt
@@ -0,0 +1,42 @@
+package com.example.watarusekiguchi.firstkotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+import kotlinx.android.synthetic.main.activity_second.*
+
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        showRandomNumber()
+    }
+
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    fun showRandomNumber() {
+        // Get the count from the intent extras
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+
+        // Generate the random number
+        val random = Random()
+        var randomInt = 0
+        // Add one because the bound is exclusive
+        if (count > 0) {
+            // Add one because the bound is exclusive
+            randomInt = random.nextInt(count + 1)
+        }
+
+        // Display the random number.
+        textview_random.text = Integer.toString(randomInt)
+
+        // Substitute the max value into the string resource
+        // for the heading, and update the heading
+        textview_label.text = getString(R.string.random_heading, count)
+    }
+
+}
